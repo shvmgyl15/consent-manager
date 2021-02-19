c4c8811800d325db3fea3d91bd624ebb9746ff56
Shivam Goel <shvmgyl15@hotmail.com>
2021-02-19 14:05:31 +0530

    TraceableMessage implements Serializable

--- commons/src/main/java/in/projecteka/library/common/TraceableMessage.java
+++ commons/src/main/java/in/projecteka/library/common/TraceableMessage.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @Builder
 @Data
-public class TraceableMessage {
+public class TraceableMessage implements Serializable {
     String correlationId;
     Object message;
 }
